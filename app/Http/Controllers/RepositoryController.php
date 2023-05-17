0830c77a711b5ba9cb7e64f8b5685f6f137ce533
AltGreed <jalvarezhz96@hotmail.com>
2023-05-16 23:12:54 -0600

    Formulario de edición

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -45,6 +45,32 @@ class RepositoryController extends Controller
         return redirect()->route('repositories.index');
     }
 
+    public function index(Request $request){
+        return view('repositories.index',[
+            'repositories' => $request->user()->repositories
+        ]);
+
+    }
+
+    public function show(Request $request, Repository $repository){
+
+        if($request->user()->id != $repository->user_id){
+            abort(403);
+        }
+
+        return view('repositories.show', compact('repository'));
+    }
+
+    public function edit(Request $request, Repository $repository){
+
+        if($request->user()->id != $repository->user_id){
+            abort(403);
+        }
+
+        return view('repositories.edit', compact('repository'));
+    }
+
+
     
 }
 
---
+++ resources/views/repositories/edit.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Editar Repositorios
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <form action="{{route('repositories.update', $repository)}}" method="POST" cñass"max-w-mg>
+                    @csrf 
+                    @method('PUT')
+
+                    <label class="block font-medium text-sm text -gray-700">URL * </label>
+                    <input class="form-input w-full rounded-md shadow-sm" type="text" name="url" value="{{$repository->url}}">
+
+                    <label class="block font-medium text-sm text -gray-700">Description * </label>
+                    <textarea class="form-input w-full rounded-md shadow-sm" type="text" name="description">  value="{{$repository->description}}"</textarea>
+
+                    <input type="submit" value="Editar" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">
+
+                    <hr class="my-4">
+
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
